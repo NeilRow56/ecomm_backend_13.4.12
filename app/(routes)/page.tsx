ea877efadab645c602c67cc148e84eb24b1fdc4c
NeilRow56 <neilrowland56@gmail.com>
2023-07-29 07:05:33 +0100

    Added billboard background component

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,8 +1,21 @@
-export default function HomePage() {
-    return (
-      <main className="">
-        Hello Store
-      </main>
-    );
-   }
-   
+import Container from "@/components/ui/container";
+import Billboard from "@/components/ui/billboard";
+import getBillboard from "@/actions/get-billboard";
+
+export const revalidate = 0
+
+import React from 'react'
+
+const HomePage = async () => {
+
+  const billboard = await getBillboard("4b57f1f9-5aad-48e4-8f7f-c029de217bbb")
+  return (
+    <Container>
+      <div>
+        <Billboard  data={billboard}/> 
+      </div>
+    </Container>
+  )
+}
+
+export default HomePage
